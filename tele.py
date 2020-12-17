5a50da3715341ca39bb6043566bbeb128e8d1799
killeravt <alexx.ss3003@gmail.com>
2020-12-18 00:33:00 +0200

    Update tele.py

--- tele.py
+++ tele.py
@@ -7,7 +7,7 @@ from pyowm.utils.config import get_default_config
 from bs4 import BeautifulSoup
 config_dict = get_default_config()
 config_dict['language'] = 'ru'
-bot = telebot.TeleBot("BOT_TOKEN")
+bot = telebot.TeleBot("1417359954:AAEWKGylMxCRYftha3N6iLjveM2M8xHHgks")
 owm = OWM('c4153a7bc6d29047fcba68583813033b', config_dict)
 
 @bot.message_handler(commands=["start"])
